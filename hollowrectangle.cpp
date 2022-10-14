499287f215093d6cc5eaa0498d5edcb1e1fe1da1
rishabh-108272 <114498105+rishabh-108272@users.noreply.github.com>
2022-10-14 21:12:45 +0530

    hollowrectangle.cpp
    Program to print hollow rectangle pattern.

---
+++ hollowrectangle.cpp
@@ -0,0 +1,19 @@
+/*Program to print hollow rectangle pattern.*/
+#include<iostream>
+using namespace std;
+int main(){
+    int row,col;
+    cout<<"Enter the rows and columns:";
+    cin>>row>>col;
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=col;j++){
+            if(i==1||i==row||j==1||j==col){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
